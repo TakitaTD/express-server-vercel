8fed8efea8667c77e040365eeee2b431c8f838a8
Tim Takita <tim@takitatd.dev>
2022-07-10 18:59:33 +1000

    beans

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const app = express();
 const port = 80;
+let requests = 0;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  requests++;
+  console.log("Request x" + requests);
+  res.send(
+    `${Math.floor(
+      Math.random() * 10000
+    )} Generated at ${new Date().toUTCString()}`
+  );
 });
 
 app.listen(port, () => {
-  console.log("Server initalised and listening at :", port);
+  console.log("Server initalised and listening at :" + port);
 });
